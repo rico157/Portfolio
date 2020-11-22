6fa8033a0babd693dd89fd398e89c57342f2c4f7
Riccardo Cogoni <cogonir@live.com>
2020-11-22 16:35:00 +0000

    add background / typography

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
