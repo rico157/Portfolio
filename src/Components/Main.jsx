59bf02620fd6ed1abd7f83dff9ceaaa831dafbec
Riccardo Cogoni <cogonir@live.com>
2020-11-30 01:52:38 +0000

    refector main page

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import SocialIcons from "./SocialIcons";
-// import BottomPage from "./BottomPage";
-import Slides from "./Slides";
-import PersonalSlides from "./PersonalSlides";
 import avatar from "../assets/avatar.png";
 import { Image } from "react-bootstrap";
-import ProjectList from "./ProjectList";
+// import BottomPage from "./BottomPage";
+// import Slides from "./Slides";
+// import PersonalSlides from "./PersonalSlides";
+// import ProjectList from "./ProjectList";
 
-export default function Main({ dim: { phone, tablet, desktop } }) {
+export default function Main({ dim: { phone, tablet, desktop, portrait } }) {
   const mainStyle = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-evenly",
     alignItems: "center",
-    marginTop: !tablet ? "0rem" : "5em",
+    marginTop: phone ? "7rem" : tablet ? "225px" : 0,
     marginBottom: "10rem"
   };
-
-  const topStyle = {
+  const screenStyle = {
+    height: tablet && portrait ? "70vh" : portrait ? "100%" : "100vh"
+  };
+  const topRowContainer = {
     // height: "80vh",
     maxWidth: "100vw",
-    width: "100vw",
-    paddingTop: "10vh",
+    // width: "100vw",
+    paddingTop: tablet ? "00vh" : "0vh",
     display: "flex",
     flexDirection: tablet ? "column" : "row",
     justifyContent: "center",
     alignItems: "center",
     marginBottom: "7em",
-    marginTop: !tablet ? "7em" : "2em"
+    marginTop: "7em"
   };
 
   const textStyle = {
@@ -35,7 +37,7 @@ export default function Main({ dim: { phone, tablet, desktop } }) {
     color: "white",
     lineHeight: 1,
     // height: tablet && "100vh",
-    width: tablet ? "80%" : "50vw",
+    width: tablet ? "80%" : "40vw",
     marginTop: tablet && "1em",
     marginLeft: !tablet && "1em",
     paddingRight: "10%",
@@ -45,31 +47,47 @@ export default function Main({ dim: { phone, tablet, desktop } }) {
     flexDirection: "column",
     justifyContent: "flex-end"
   };
+
+  const imageContainer = {
+    width: "40vw",
+    display: "flex",
+    justifyContent: tablet ? "center" : "flex-end",
+    marginTop: phone && !tablet && "10%"
+  };
+
+  const avatarStyle = {
+    marginBottom: tablet && "3em",
+    width: "300px",
+    height: "300px",
+    borderRadius: "50%",
+    border: "3px solid",
+    backgroundColor: "#ffffff99",
+    boxShadow:
+      "9px 9px 20px rgba(8, 25, 36, 0.562), -9px -9px 20px rgba(66, 133, 161, 0.452)"
+  };
+
+  const projectsStyle = {
+    marginTop: "10em",
+    marginLeft: !tablet && "3em",
+    marginRight: !tablet && "3em",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    // maxWidth: "fit-content",
+    backgroundColor: "#00000077",
+    borderRadius: "1em",
+    height: "fit-content",
+    width: phone ? "100%" : tablet ? "90%" : desktop ? "90%" : "80%"
+    // padding: "2em"
+  };
+
   return (
     <div style={mainStyle}>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <div style={topStyle}>
-          <div
-            style={{
-              width: "50vw",
-              display: "flex",
-              justifyContent: tablet ? "center" : "flex-end"
-            }}
-          >
-            <Image
-              src={avatar}
-              roundedCircle
-              style={{
-                marginBottom: tablet && "3em",
-                width: "300px",
-                height: "300px",
-                borderRadius: "50%",
-                border: "3px solid",
-                backgroundColor: "#ffffff99",
-                boxShadow:
-                  "9px 9px 20px rgba(8, 25, 36, 0.562), -9px -9px 20px rgba(66, 133, 161, 0.452)"
-              }}
-            />
+      <div style={screenStyle} className="centerCol">
+        <div style={topRowContainer}>
+          <div style={imageContainer}>
+            <Image src={avatar} roundedCircle style={avatarStyle} />
           </div>
           <div className="text" style={textStyle}>
             <h2 style={{ margin: 0, marginLeft: "1rem", lineHeight: 1 }}>
@@ -112,27 +130,11 @@ export default function Main({ dim: { phone, tablet, desktop } }) {
             </p>
           </div>
         </div>
-      </div>
 
-      {/* <BottomPage dim={{ phone, tablet }}> */}
-      <SocialIcons dim={{ phone, tablet }} />
-      <div
-        style={{
-          marginTop: "10em",
-          marginLeft: !tablet && "3em",
-          marginRight: !tablet && "3em",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          // maxWidth: "fit-content",
-          backgroundColor: "#00000077",
-          borderRadius: "1em",
-          height: "fit-content",
-          width: phone ? "100%" : tablet ? "90%" : desktop ? "90%" : "80%"
-          // padding: "2em"
-        }}
-      >
+        {/* <BottomPage dim={{ phone, tablet }}> */}
+        <SocialIcons dim={{ phone, tablet }} />
+      </div>
+      <div style={projectsStyle}>
         <h2 style={{ color: "white", textAlign: "center", marginTop: "1em" }}>
           Recent projects
         </h2>
