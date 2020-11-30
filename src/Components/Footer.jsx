b656aaec8d873f0d80e695d5599a9175197dd2ff
Riccardo Cogoni <cogonir@live.com>
2020-11-30 23:48:06 +0000

    update footer message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,8 +10,14 @@ export default function Footer() {
   return (
     <Navbar style={footer} className="m-auto sticky-top">
       <Nav className="flex-column m-auto" style={textStyle}>
-        <h1 style={{ margin: "2em 0 0 0" }}>Thank you </h1>
-        <h2 style={{ margin: "0 0 2em 1em" }}>for visiting my website</h2>
+        <h1 style={{ margin: "2em 0 0 0" }}>Thank you for your time, </h1>
+        <h2 style={{ margin: "0 0 2em 1em" }}>
+          {" "}
+          have a nice day!{" "}
+          <span role="img" aria-label="down">
+            😌{" "}
+          </span>
+        </h2>
         <Button
           variant="outline-light"
           style={{
